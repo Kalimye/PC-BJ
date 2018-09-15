31bebaff677d3323aab71efc87c31e86040b0817
Kalimye <1991818243@qq.com>
2018-09-15 10:58:25 +0800

    优质干货中的标题分类部分渲染到页面

--- pc/js/index.js
+++ pc/js/index.js
@@ -1,3 +1,34 @@
+// console.log(article);
+
+
+var article_tab = article.filter(function (item) {
+    return item.name != "全部文章";
+});
+
+
+console.log(article_tab);
+
+var btnWrap = document.querySelector(".excellent-btn ul");
+
+article_tab.forEach(item => {
+
+    var btnLi = document.createElement("li");
+    var btnLink = document.createElement("a");
+
+    btnLink.href = "javascript:;";
+    btnLink.textContent = item.name;
+
+    btnLi.appendChild(btnLink);
+
+    btnWrap.appendChild(btnLi);
+    // console.log(item)
+});
+
+// 给第一个li加上class
+btnWrap.firstElementChild.classList.add("active");
+
+// 优质干货tab切换 
+
 var btn = document.querySelectorAll(".excellent-btn li");
 var con = document.querySelectorAll(".excellent-con-list");
 
